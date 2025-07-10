6f9ebb5ebed52af3955102ffab97689ff5673c7e
Alexander D. Macri <admfav2012@gmail.com>
2025-07-10 17:56:17 -0400

    stuff

--- LeetCode/3401-3500/Proble3440_RescheduleMeetingsforMaximumFreeTimeII/RescheduleMeetingsforMaximumFreeTimeII.cpp
+++ LeetCode/3401-3500/Proble3440_RescheduleMeetingsforMaximumFreeTimeII/RescheduleMeetingsforMaximumFreeTimeII.cpp
@@ -1,43 +1,42 @@
 class Solution {
-  public:
-      int maxFreeTime(int eventTime, vector<int>& start, vector<int>& end) {
-          int n = (int)start.size();
-          vector<int> gaps(n + 1);
-  
-          gaps[0] = start[0];
-          for (int i = 1; i < n; ++i)
-              gaps[i] = start[i] - end[i - 1];
-          gaps[n] = eventTime - end[n - 1];
-  
-          vector<int> prefixMax(n + 2, 0), suffixMax(n + 2, 0);
-          for (int i = 1; i <= n + 1; ++i)
-              prefixMax[i] = max(prefixMax[i - 1], gaps[i - 1]);
-          for (int i = n; i >= 0; --i)
-              suffixMax[i] = max(suffixMax[i + 1], gaps[i]);
-  
-          int maxFree = *max_element(gaps.begin(), gaps.end());
-  
-          for (int i = 0; i < n; ++i) {
-              int dur = end[i] - start[i];
-  
-              int left = (i == 0) ? 0 : end[i - 1];
-              int right = (i == n - 1) ? eventTime : start[i + 1];
-              int mergedGap = right - left;
-  
-              int largestElsewhereGap = max(prefixMax[i], suffixMax[i + 2]);
-  
-              int leftoverInMergedGap = 0;
-              if (mergedGap >= dur) {
-                  leftoverInMergedGap = max(right - (left + dur), (right - dur) - left);
-              }
-  
-              int leftoverElsewhere = (largestElsewhereGap >= dur) ? (largestElsewhereGap - dur) : 0;
-  
-              int candidate = max(leftoverInMergedGap, leftoverElsewhere);
-              maxFree = max(maxFree, candidate);
-          }
-  
-          return maxFree;
-      }
-  };
-  
+    public:
+        int maxFreeTime(int eventTime, vector<int>& start, vector<int>& end) {
+            int n = (int)start.size();
+            vector<long long> gaps(n + 1);
+    
+            gaps[0] = start[0];
+            for (int i = 1; i < n; ++i)
+                gaps[i] = start[i] - end[i - 1];
+            gaps[n] = (long long)eventTime - end[n - 1];
+    
+            vector<long long> prefixMax(n + 2, 0), suffixMax(n + 2, 0);
+            for (int i = 1; i <= n + 1; ++i)
+                prefixMax[i] = max(prefixMax[i - 1], gaps[i - 1]);
+            for (int i = n; i >= 0; --i)
+                suffixMax[i] = max(suffixMax[i + 1], gaps[i]);
+    
+            long long maxFree = *max_element(gaps.begin(), gaps.end());
+    
+            for (int i = 0; i < n; ++i) {
+                long long dur = end[i] - start[i];
+    
+                long long mergedGap = gaps[i] + dur + gaps[i + 1];
+    
+                long long largestElsewhereGap = max(prefixMax[i], suffixMax[i + 2]);
+    
+                long long leftoverElsewhere = (largestElsewhereGap >= dur) ? (largestElsewhereGap - dur) : -1;
+    
+                long long candidate;
+                if (leftoverElsewhere >= 0) {
+                    candidate = max(mergedGap, leftoverElsewhere);
+                } else {
+                    candidate = mergedGap - dur;
+                }
+    
+                maxFree = max(maxFree, candidate);
+            }
+    
+            return (int)maxFree;
+        }
+    };
+    
