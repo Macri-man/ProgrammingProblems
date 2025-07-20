a7d8fa1f3b5cfaa00c4169d71ac76c05b4c50993
Alexander D. Macri <admfav2012@gmail.com>
2025-07-19 22:29:30 -0400

    stuff

---
+++ LeetCode/1201-1300/Problem1233_RemoveSubFoldersfromtheFilesystem/RemoveSubFoldersfromtheFilesystem.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(), folder.end());
+
+        vector<string> result;
+
+        for (const string& f : folder) {
+            if (result.empty() || f.compare(0, result.back().size(), result.back()) != 0 || f[result.back().size()] != '/') {
+                result.push_back(f);
+            }
+        }
+
+        return result;
+    }
+};
